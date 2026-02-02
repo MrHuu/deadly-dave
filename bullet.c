ec37e03bffa5453b44d47b0fc18bdc627f181f37
MrHuu <wilbert.huu@gmail.com>
2026-02-02 22:35:40 +0100

    Initialize bullet state

--- bullet.c
+++ bullet.c
@@ -75,6 +75,7 @@ void bullet_destroy(bullet_t *bullet) {
 bullet_t* bullet_create_internal(int x, int y, int speed) {
     bullet_t *bullet = malloc(sizeof(bullet_t));
     bullet->speed_x = speed;
+    bullet->state = 0;
     bullet->steps = 0;
     bullet->tick = &bullet_tick;
     bullet->is_dead = &bullet_is_dead;
